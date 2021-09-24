7dde8deb8d101a0f5ad783725d7f4d038658901a
MatheusCoutinho10 <coutinho.mh@outlook.com.br>
2021-09-24 15:04:43 -0300

    Pegando informações do usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    private $loggedUser;
+
+    public function __construct(){
+        $this->middleware('auth:api'); //Tem que estar logado para realizar o processo
+        $this->loggedUser = auth()->user(); //Informações do usuário que está logado
+    }
+
+
+    //Método que pega as informações do usuário
+    public function read(){  
+        $array = ['error' => ''];
+
+        $info = $this->loggedUser; //Pega as informações
+        $info['avatar'] = url('media/avatars/'.$info['avatar']); //Corrigindo
+        $array['data'] = $info; //Mandando pro data
+
+        return $array;
+    }
 }
